a17e130e94adbd7d220977ac73e18ef344a17f84
orisa07037616291 <orisakwepeter@gmail.com>
2022-03-09 14:37:51 +0100

    my first commit

---
+++ src/data/category.jsx
@@ -0,0 +1,16 @@
+const categories = [
+  { value: "English", label: "en" },
+  { value: "Hindi", label: "hi" },
+  { value: "Spanish", label: "es" },
+  { value: "French", label: "fr" },
+  { value: "Russian", label: "ru" },
+  { value: "Japanese", label: "ja" },
+  { value: "German", label: "de" },
+  { value: "Italian", label: "it" },
+  { value: "Korean", label: "ko" },
+  { value: "Brazilian Portuguese", label: "pt-BR" },
+  { value: "Arabic", label: "ar" },
+  { value: "Turkish", label: "tr" },
+];
+
+export default categories;
